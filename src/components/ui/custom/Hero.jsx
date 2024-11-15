56138f701b19020080497cde003ed26c13ec109b
harshfreikin <harshtiwari8880@gmail.com>
2024-11-15 14:18:28 +0530

    something something

--- src/components/ui/custom/Hero.jsx
+++ src/components/ui/custom/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button } from '../button'
+import { Link } from 'react-router-dom'
 
 function Hero() {
   return (
@@ -8,7 +9,9 @@ function Hero() {
       className='font-extrabold text-[50px] text-center mt-16'
       ><span className='text-red-600'>Discover Your Next Adventure with AI:</span> <br></br>Personalize Itinerares at Your Fingertips</h1>
       <p className='text-xl text-gray-500 text-center'>Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.</p>
+      <Link to='/create-trip'>
       <Button> Get Started,It's Free</Button>
+      </Link>
     </div>
   )
 }
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 
 function CreateTrip() {
   return (
-    <div>
-      craeteTrip
+    <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
+      <h2 className='font-bold text-3xl'>Tell us your travel preferences 🏕️🌴</h2>
+      <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information, and our trip planner will generate a customized itinerary based on your preferences.</p>
+    
+    <div className='mt-20'>
+      <div>
+        <h2 className='font-bold'>What is Destination of choice?</h2>
+       
+      
+      </div>
+    </div>
+    
     </div>
   )
 }
