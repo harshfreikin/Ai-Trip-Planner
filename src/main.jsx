7111bcbc7bdd8e7322dd9b014031af794b350127
harshfreikin <harshtiwari8880@gmail.com>
2024-11-17 00:36:22 +0530

    new things added

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import CreateTrip from './create-trip/index.jsx'
-import Header from './components/ui/custom/Header'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CreateTrip from "./create-trip/index.jsx";
+import Header from "./components/ui/custom/Header";
 
 const router = createBrowserRouter([
   {
-    path: '/', // corrected from 'patch' to 'path'
-    element: <App />
+    path: "/", // corrected from 'patch' to 'path'
+    element: <App />,
   },
   {
-    path: '/create-trip', // corrected 'createTrip' to 'CreateTrip'
-    element: <CreateTrip />
-  }
-])
+    path: "/create-trip", // corrected 'createTrip' to 'CreateTrip'
+    element: <CreateTrip />,
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Header />
-    <RouterProvider router={router} /> {/* corrected from 'route' to 'router' */}
+    <RouterProvider router={router} />{" "}
+    {/* corrected from 'route' to 'router' */}
   </StrictMode>
-)
+);
