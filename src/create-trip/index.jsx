8b4764e7ded8413a9477883151f67ecf76590c85
harshfreikin <harshtiwari8880@gmail.com>
2024-11-27 15:27:24 +0530

    front-end completed

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,10 +1,29 @@
 import { Button } from "@/components/ui/button";
 import { SelectBudgetOptions, SelectTravelesList } from "@/constants/options";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
 function CreateTrip() {
   const [place, setplace] = useState();
+
+  const [formData, setFormData] = useState([]);
+
+  const handleInputChange = (name, value) => {
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  useEffect(() => {
+    console.log(formData);
+  }, [formData]);
+
+  const onGenerateTrip = () => {
+    if (formData?.noOfDays > 10) {
+      return;
+    }
+    console.log(formData);
+  };
 
   return (
     <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
@@ -27,30 +46,36 @@ function CreateTrip() {
               value: place,
               onChange: (v) => {
                 setplace(v);
-                console.log(v);
+                handleInputChange("location", v);
               },
             }}
           />
         </div>
         <div>
-          <label htmlFor="trip-days" className="block text-xl my-3 font-medium">
+          <h2 htmlFor="trip-days" className=" text-xl my-3 font-medium">
             How many days are you planning for your trip?
-          </label>
+          </h2>
           <input
             id="trip-days"
             placeholder="Ex. 3"
             type="number"
             className="border border-gray-300 p-2 rounded w-full bg-white"
+            onChange={(e) => handleInputChange("noOfDays", e.target.value)}
           />
         </div>
       </div>
+      <br></br><br></br>
       <div>
         <h2 className="text-xl my-3 font-medium">What is Your Budget?</h2>
         <div className="grid grid-cols-3 gap-5 mt-5">
           {SelectBudgetOptions.map((item, index) => (
             <div
               key={index}
-              className="p-4 border cursor-pointer rounded-lg hover:shadow-lg"
+              onClick={() => handleInputChange("Budget", item.title)}
+              className={`p-4 border cursor-pointer
+               rounded-lg hover:shadow-lg
+               ${formData?.Budget == item.title && "shadow-lg border-black"}
+               `}
             >
               <h2 className="text-4xl">{item.icon}</h2>
               <h2 className="font-bold text-lg">{item.title}</h2>
@@ -58,6 +83,7 @@ function CreateTrip() {
             </div>
           ))}
         </div>
+        <br></br><br></br>
         <div>
           <h2 className="text-xl my-3 font-medium">
             Who do you plan on traveling with on your next adventure?
@@ -66,7 +92,13 @@ function CreateTrip() {
             {SelectTravelesList.map((item, index) => (
               <div
                 key={index}
-                className="p-4 border cursor-pointer rounded-lg hover:shadow-lg"
+                onClick={() => handleInputChange("Traveler", item.people)}
+                className={`p-4 border cursor-pointer
+                 rounded-lg hover:shadow-lg
+                 ${
+                   formData?.Traveler == item.people && "shadow-lg border-black"
+                 }
+                `}
               >
                 <h2 className="text-4xl">{item.icon}</h2>
                 <h2 className="font-bold text-lg">{item.title}</h2>
@@ -77,8 +109,9 @@ function CreateTrip() {
         </div>
       </div>
       <div className="my-10 justify-end flex ">
-        <Button>Generate Trip</Button>
+        <Button onClick>Generate Trip</Button>
       </div>
+      <br></br>
     </div>
   );
 }
